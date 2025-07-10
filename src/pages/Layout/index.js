0e725c9c5bdfa0d79bc548e0dc7f693a691eb863
tom <tom@konjapower.com>
2025-07-10 10:17:35 +0800

    feature/tabbar 切换菜单

--- src/pages/Layout/index.js
+++ src/pages/Layout/index.js
@@ -1,15 +1,57 @@
-import { Outlet } from "react-router-dom"
-import { Button } from "antd-mobile"
+import { Outlet,useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { useEffect } from "react";
+import { getBillList } from "../../store/modules/billStore.js";
+import { TabBar } from "antd-mobile";
+import {
+  BillOutline,
+  AddCircleOutline,
+  CalculatorOutline,
+} from "antd-mobile-icons";
+import "./index.scss";
 
-function Layout(){
-    return(
-        <div>
-            <Outlet/>
-            <h1>Layout</h1>
-            <p>Layout</p>
-            <Button color="primary">Primary Button</Button>
-        </div>
-    )
+const tabs = [
+  {
+    key: "/month",
+    title: "月度账单",
+    icon: <BillOutline />,
+  },
+  {
+    key: "/new",
+    title: "记账",
+    icon: <AddCircleOutline />,
+  },
+  {
+    key: "/year",
+    title: "年度账单",
+    icon: <CalculatorOutline />,
+  },
+];
+
+function Layout() {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getBillList());
+  }, [dispatch]);
+  const navigate = useNavigate();
+  const switchRouter = (path) => {
+    navigate(path);
+  };
+  return (
+    <div className="layout">
+      <div className="container">
+        <Outlet />
+      </div>
+
+      <div className="footer">
+        <TabBar onChange={switchRouter}>
+          {tabs.map((item) => (
+            <TabBar.Item key={item.key} icon={item.icon} title={item.title} />
+          ))}
+        </TabBar>
+      </div>
+    </div>
+  );
 }
 
-export default Layout
+export default Layout;
